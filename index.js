bf9fa72cf51f8d3a33476d9b29730ddfa290c2fa
emmanuelcardona1 <163427104+emmanuelcardona1@users.noreply.github.com>
2024-09-19 07:19:31 -0500

    agrego la vista de ingreso

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 function ingresar() {
-    let usuario = document.getElementById("usuario").value;
-    let contraseña = document.getElementById("contraseña").value;
-    let verificacionusuario1 = "manolo1";
-    let verificacioncontraseña1 = "123456" ;
-    let verificacionusuario2 = "admin";
-    let verificacioncontraseña2 = "admin1234" ;
+  let usuario = document.getElementById("usuario").value;
+  let contraseña = document.getElementById("contraseña").value;
+  let verificacionusuario1 = "admin1";
+  let verificacioncontraseña1 = "123456";
+  let verificacionusuario2 = "admin";
+  let verificacioncontraseña2 = "admin1234";
 
-    // Verificación de usuario y contraseña
-    if (usuario === verificacionusuario1 && contraseña === verificacioncontraseña1 || usuario === verificacionusuario2 && contraseña === verificacioncontraseña2) {
-        // Redirige a otra página si las credenciales son correctas
-        window.location.href = "./prueba.html";
-    } else {
-        // Muestra alerta si las credenciales son incorrectas
-        alert("Usuario o contraseña inválidos");
-    }
+  // Verificación de usuario y contraseña
+  if (
+    (usuario === verificacionusuario1 &&
+      contraseña === verificacioncontraseña1) ||
+    (usuario === verificacionusuario2 && contraseña === verificacioncontraseña2)
+  ) {
+    // Redirige a otra página si las credenciales son correctas
+    window.location.href = "./ingreso.html";
+  } else {
+    // Muestra alerta si las credenciales son incorrectas
+    alert("Usuario o contraseña inválidos");
+  }
 }
-
- 

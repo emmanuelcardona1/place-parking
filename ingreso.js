f5cf242c1711b9caf6d017156801ead9323825cb
juan guapacha <juandavidguapachavillegas@gmail.com>
2024-10-17 14:34:10 -0500

    avance 17/10/2024

--- ingreso.js
+++ ingreso.js
@@ -1,22 +1,30 @@
-let ingreso = [];
+let ingresos = [];
 
 function entrada() {
   let placa = document.getElementById("placa").value;
-  let carro = document.getElementById("carro").value;
-  let moto = document.getElementById("moto").value;
-  let hibrido = document.getElementById("hibrido").value;
+  let tipo = document.getElementById("tipo").value;
   let hora = new Date();
   let horaActual = hora.toLocaleTimeString();
   let datos = {
     placa: placa,
-    carro: carro,
-    moto: moto,
-    hibrido: hibrido,
+    tipo:tipo,
     hora: horaActual,
   };
 
-  ingreso.push(datos);
-  console.log(ingreso);
+  ingresos.push(datos);
+  console.log(ingresos);
+
+  let  tabla = document.getElementById("tabla").getElementsByTagName("tbody")[0];
+  let fila = tabla.insertRow();
+  let celdaPlaca = fila.insertCell();
+  let celdaTipo = fila.insertCell();
+  let celdaHora = fila.insertCell();
+
+  // Asignar los valores a las celdas
+  celdaPlaca.textContent = datos.placa;
+  celdaTipo.textContent = datos.tipo;
+  celdaHora.textContent = datos.hora;
+
 }
  
 
